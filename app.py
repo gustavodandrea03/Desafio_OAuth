40bcffc4121597bd061f4d339b73fd9ae6e864c7
gustavodandrea03 <162323213+gustavodandrea03@users.noreply.github.com>
2024-07-09 17:30:13 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from authlib.integrations.flask_client import OAuth
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'SECRET_KEY')
@@ -11,17 +11,12 @@ app.secret_key = os.getenv('SECRET_KEY', 'SECRET_KEY')
 oauth = OAuth(app)
 github = oauth.register(
     name='github',
-    client_id=os.getenv('CLIENT ID'),
-    client_secret=os.getenv('CLIENT SECRET'),
+    client_id=os.getenv('CLIENT_ID'),
+    client_secret=os.getenv('CLIENT_SECRET'),
     authorize_url='https://github.com/login/oauth/authorize',
-    authorize_params=None,
     access_token_url='https://github.com/login/oauth/access_token',
-    access_token_params=None,
-    refresh_token_url=None,
-    redirect_uri=None,
     client_kwargs={'scope': 'user:email'},
 )
-
 
 @app.route('/')
 def index():
@@ -34,7 +29,7 @@ def login():
 
 @app.route('/logout')
 def logout():
-    session.pop('token', None)
+    session.pop('user', None)
     return redirect('/')
 
 @app.route('/login/authorized')
@@ -44,7 +39,6 @@ def authorized():
     user_info = resp.json()
     session['user'] = user_info
     return f"Logged in as: {user_info['login']}"
-
 
 if __name__ == '__main__':
     app.run(debug=True)
